69e81f7d8a9c8d0054003f62aa447bba5865b03e
Sarath Kumar <161195437+SarathuOfficial@users.noreply.github.com>
2025-01-15 14:12:15 +0530

    Time: 1 ms (15.09%), Space: 40.6 MB (46.23%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        int setbits = Integer.bitCount(num2);
+
+        String binaryString = Integer.toBinaryString(num1);
+        StringBuilder res = new StringBuilder(binaryString);
+
+        for (int i = 0; i < res.length(); i++) {
+            if (res.charAt(i) == '1' && setbits > 0) {
+                setbits--; 
+            } else {
+                res.setCharAt(i, '0'); 
+            }
+        }
+
+        int i = res.length() - 1;
+        while (setbits > 0 && i >= 0) {
+            if (res.charAt(i) == '0') {
+                res.setCharAt(i, '1');
+                setbits--;
+            }
+            i--;
+        }
+
+        while (setbits > 0) {
+            res.insert(0, '1');
+            setbits--;
+        }
+
+        return Integer.parseInt(res.toString(), 2);
+    }
+}
