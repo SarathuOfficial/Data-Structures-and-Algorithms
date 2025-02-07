3dedaf3092bb86a3c8a865fb93ad4745f6d23e73
Sarath Kumar <161195437+SarathuOfficial@users.noreply.github.com>
2025-02-07 17:14:54 +0530

    Time: 195 ms (74.52%), Space: 68.9 MB (59.24%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.java
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -5,7 +5,7 @@ class Solution {
 
     public int tupleSameProduct(int[] nums) {
         int n = nums.length;
-        HashMap<Integer, Integer> freq = new HashMap<>(); // K -> product, V -> frequency of such pair
+        HashMap<Integer, Integer> freq = new HashMap<>(); 
 
         for (int i = 0; i < n - 1; ++i) {
             for (int j = i + 1; j < n; ++j) {
