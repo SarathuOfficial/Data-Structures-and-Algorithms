3908299c3e17189df31cf6cf1bbc49ccc216e20b
Sarath Kumar <161195437+SarathuOfficial@users.noreply.github.com>
2025-02-16 22:21:11 +0530

    Time: 0 ms (100%), Space: 41.2 MB (40.15%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
@@ -1,39 +1,35 @@
 class Solution {
-    public int[] constructDistancedSequence(int n) {
-        int[] result = new int[2 * n - 1];
-        boolean[] used = new boolean[n + 1];
-        backtrack(result, used, n, 0);
-        return result;
-    }
-    private boolean backtrack(int[] result, boolean[] used, int n, int index){
-        while(index < result.length && result[index] != 0){
-            index++;
-        }
-        if(index == result.length){
-            return true;
-        }
-        for(int i = n; i >= 1; i--){
-            if(used[i]) continue;
+    private boolean findLargestArray(int[] res, boolean[] used, int pos, int n) {
+        if (pos == 2 * n - 1) return true;
+        if (res[pos] != 0) 
+            return findLargestArray(res, used, pos + 1, n);
 
-            if(i == 1){
-                result[index] = 1;
-                used[1] = true;
-                if(backtrack(result, used, n, index + 1)) return true;
-                result[index] = 0;
-                used[1] = false;
+        for (int i = n; i >= 1; --i) { 
+            if (used[i]) continue;
+
+            used[i] = true;
+            res[pos] = i;
+            if (i == 1 && findLargestArray(res, used, pos + 1, n))
+                return true;
+            if (i > 1 && (pos + i) < (2 * n - 1) && res[pos + i] == 0) {
+                res[pos + i] = i;
+                if (findLargestArray(res, used, pos + 1, n))
+                    return true;
+                res[pos + i] = 0;
             }
-            else{
-                if(index + i < result.length && result[index + i] == 0){
-                    result[index] = i;
-                    result[index + i] = i;
-                    used[i] = true;
-                    if(backtrack(result, used, n, index + 1)) return true;
-                    result[index] = 0;
-                    result[index + i] = 0;
-                    used[i] = false;
-                }
-            }
+            used[i] = false;
+            res[pos] = 0;
         }
         return false;
+    }
+
+    public int[] constructDistancedSequence(int n) {
+        int[] res = new int[2 * n - 1];
+        boolean[] used = new boolean[n + 1];
+        Arrays.fill(res, 0);
+        Arrays.fill(used, false);
+
+        findLargestArray(res, used, 0, n);
+        return res;
     }
 }
