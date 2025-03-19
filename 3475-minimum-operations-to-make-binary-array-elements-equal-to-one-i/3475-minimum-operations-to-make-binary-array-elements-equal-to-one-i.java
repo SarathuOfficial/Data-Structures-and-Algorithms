6531ae8024e95b499fc5b40c43345d6b9c932323
Sarath Kumar <161195437+SarathuOfficial@users.noreply.github.com>
2025-03-19 23:55:22 +0530

    Time: 6 ms (100%), Space: 57.5 MB (49.49%) - LeetHub

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    private void flipWindow(int[] nums, int pos) {
+        nums[pos] ^= 1;       // Flip the bit at position `pos`
+        nums[pos + 1] ^= 1;   // Flip the bit at position `pos + 1`
+        nums[pos + 2] ^= 1;   // Flip the bit at position `pos + 2`
+    }
+
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int operations = 0;
+
+        for (int i = 0; i < n - 2; ++i) {
+            if (nums[i] == 1) {
+                continue; 
+            }
+            flipWindow(nums, i); 
+            operations++;      
+        }
+
+        if (nums[n - 2] == 0 || nums[n - 1] == 0) {
+            return -1; // If not, return -1
+        }
+
+        return operations; 
+    }
+}
